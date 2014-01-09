f7634fdf95702e33d57af6eeeeb99eaa94d4dc26
Senthil <psenthil202@gmail.com>
2014-01-10 01:22:10 +0530

    Index removed

--- app/indices/movie_index.rb
+++ app/indices/movie_index.rb
@@ -1,7 +1,5 @@
 ThinkingSphinx::Index.define :movie, :with => :active_record do
   indexes writer, :sortable => true
-  indexes writer
 	indexes description
 	indexes released_on
-  #indexes description
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Movie.create(:name => "Jilla" , :director => "Vijay", :released_on => "Jan 10", :description => "Most expected.")
